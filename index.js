a06b74ec2c1f5b1e4ec6841965fe4ac127eba419
Mehedi Hassan <rehan51322@gmail.com>
2022-05-28 09:35:22 +0600

    products delete added

--- index.js
+++ index.js
@@ -225,6 +225,13 @@ async function run() {
       const result = await orderCollection.deleteOne(query);
       res.send(result);
     });
+
+    app.delete("/parts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await partsCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
